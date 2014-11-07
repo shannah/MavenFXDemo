6f6d1bfa5e9ad32d6561e46e8af8d5489eb3f329
shannah <steve@weblite.ca>
2014-11-07 13:08:55 -0800

    After activating mirah and adding NewMirahClass with hello world

--- src/main/java/com/mycompany/mavenfxdemo/FXMLController.java
+++ src/main/java/com/mycompany/mavenfxdemo/FXMLController.java
@@ -16,6 +16,7 @@ public class FXMLController implements Initializable {
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        new NewMirahClass().helloWorld();
     }
     
     @Override
---
+++ src/main/java/com/mycompany/mavenfxdemo/NewMirahClass.mirah
@@ -0,0 +1,8 @@
+package com.mycompany.mavenfxdemo
+
+class NewMirahClass 
+  def helloWorld
+    puts 'hello from Mirah!'
+  end
+end
+
